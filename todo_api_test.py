6990dfdb6c87baaea73da349d14b6d856892d25f
Peter Aarestad <paarestad@grubhub.com>
2015-08-02 21:08:05 -0500

    more unit tests!

--- todo_api_test.py
+++ todo_api_test.py
@@ -1,44 +1,41 @@
 import unittest
-
 import todo_api
-
 import sqlite3
+import json
 
 todo_api.DATABASE = './todo_test.db'
 
 
 class TodoApiTest(unittest.TestCase):
     def setUp(self):
-        db = sqlite3.connect(todo_api.DATABASE)
+        self.app = todo_api.app.test_client()
 
-        db.execute('DROP TABLE IF EXISTS todo_items')
+        self.db = sqlite3.connect(todo_api.DATABASE)
+        self.c = self.db.cursor()
+
+        self.db.execute('DROP TABLE IF EXISTS todo_items')
 
         with open('todo_schema.sql') as schema:
             script = schema.read()
-            db.executescript(script)
-            db.commit()
+            self.db.executescript(script)
+            self.db.commit()
 
     def test_save_todo_item(self):
-        db = sqlite3.connect(todo_api.DATABASE)
-        c = db.cursor()
-
-        self.assertEqual(0, c.execute('select count(*) from todo_items').fetchone()[0])
+        self.assertEqual(0, self.c.execute('select count(*) from todo_items').fetchone()[0])
 
         new_todo_item = todo_api.TodoItem('peter', 'Get the milk', 1234567890)
 
-        new_todo_item = todo_api.save_todo_item(new_todo_item, db)
+        new_todo_item = todo_api.save_todo_item(new_todo_item, self.db)
 
         self.assertEqual(('peter', 'Get the milk', 1234567890, new_todo_item.id, False),
-                         c.execute('SELECT username, description, due_date, id, completed '
-                                   'FROM todo_items WHERE id = ?', (new_todo_item.id,)).fetchone())
+                         self.c.execute('SELECT username, description, due_date, id, completed '
+                                        'FROM todo_items WHERE id = ?', (new_todo_item.id,)).fetchone())
 
     def test_find_todo_item_by_id(self):
-        db = sqlite3.connect(todo_api.DATABASE)
-
         new_todo_item = todo_api.TodoItem('peter', 'Get the milk', 1234567890)
-        todo_api.save_todo_item(new_todo_item, db)
+        todo_api.save_todo_item(new_todo_item, self.db)
 
-        fetched_todo_item = todo_api.find_todo_item_by_id(new_todo_item.id, db)
+        fetched_todo_item = todo_api.find_todo_item_by_id(new_todo_item.id, self.db)
 
         self.assertEqual(new_todo_item.id, fetched_todo_item.id)
         self.assertEqual(new_todo_item.username, fetched_todo_item.username)
@@ -47,17 +44,15 @@ class TodoApiTest(unittest.TestCase):
         self.assertEqual(new_todo_item.completed, fetched_todo_item.completed)
 
     def test_find_todo_items_by_username(self):
-        db = sqlite3.connect(todo_api.DATABASE)
-
         new_todo_item_1 = todo_api.TodoItem('peter', 'Get the milk', 1234567890, None, True)
         new_todo_item_2 = todo_api.TodoItem('peter', 'Get the bread', 1234567890, None, False)
-        todo_api.save_todo_item(new_todo_item_1, db)
-        todo_api.save_todo_item(new_todo_item_2, db)
+        todo_api.save_todo_item(new_todo_item_1, self.db)
+        todo_api.save_todo_item(new_todo_item_2, self.db)
 
-        all_by_username = todo_api.find_todo_items_by_username('peter', db)
+        all_by_username = todo_api.find_todo_items_by_username('peter', self.db)
         self.assertEqual(2, len(all_by_username))
 
-        uncompleted_by_username = todo_api.find_todo_items_by_username('peter', db, True)
+        uncompleted_by_username = todo_api.find_todo_items_by_username('peter', self.db, True)
         self.assertEqual(1, len(uncompleted_by_username))
 
         uncompleted = uncompleted_by_username[0]
@@ -65,6 +60,50 @@ class TodoApiTest(unittest.TestCase):
         self.assertEqual(uncompleted.description, 'Get the bread')
         self.assertEqual(uncompleted.due_date, 1234567890)
         self.assertEqual(uncompleted.completed, False)
+
+    def test_todos_for_user_flask(self):
+        new_todo_item_1 = todo_api.TodoItem('peter', 'Get the milk', 1234567890, None, True)
+        new_todo_item_2 = todo_api.TodoItem('peter', 'Get the bread', 1234567890, None, False)
+        todo_api.save_todo_item(new_todo_item_1, self.db)
+        todo_api.save_todo_item(new_todo_item_2, self.db)
+
+        json_response = self.app.get('/todo-api/v1/todos/peter/').data
+        todos = json.loads(json_response)['todo_items']
+        self.assertEqual(2, len(todos))
+
+    def test_uncompleted_todos_for_user_flask(self):
+        new_todo_item_1 = todo_api.TodoItem('peter', 'Get the milk', 1234567890, None, True)
+        new_todo_item_2 = todo_api.TodoItem('peter', 'Get the bread', 1234567890, None, False)
+        todo_api.save_todo_item(new_todo_item_1, self.db)
+        todo_api.save_todo_item(new_todo_item_2, self.db)
+
+        json_response = self.app.get('/todo-api/v1/todos/peter/uncompleted').data
+        todos = json.loads(json_response)['uncompleted_todo_items']
+        self.assertEqual(1, len(todos))
+
+    def test_save_new_todo_flask(self):
+        new_todo = {'username': 'peter', 'description': 'Get the milk', 'due_date': 1438776000}
+        response = self.app.post('/todo-api/v1/todo/', data=json.dumps(new_todo), content_type='application/json')
+
+        new_todo_response = json.loads(response.data)
+        self.assertEqual(False, new_todo_response['completed'])
+        self.assertEqual('Get the milk', new_todo_response['description'])
+        self.assertEqual(1438776000, new_todo_response['due_date'])
+        self.assertEqual('peter', new_todo_response['username'])
+        self.assertEqual(1, new_todo_response['id'])
+
+    def todo_item_complete_flask(self):
+        todo_item = todo_api.TodoItem('peter', 'Get the bread', 1234567890, None, False)
+        todo_item = todo_api.save_todo_item(todo_item, self.db)
+
+        response = self.app.post('/todo-api/v1/todo/' + todo_item.id + '/complete')
+
+        todo_response = json.loads(response.data)
+        self.assertEqual(True, todo_response['completed'])
+        self.assertEqual('Get the milk', todo_response['description'])
+        self.assertEqual(1234567890, todo_response['due_date'])
+        self.assertEqual('peter', todo_response['username'])
+        self.assertEqual(todo_item.id, todo_response['id'])
 
 
 if __name__ == '__main__':
